69d02ef2cdfba2a01d624d822d4045fd77491475
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-03-25 23:30:34 +0200

    added styles and scripts for  footer, rewrite footer styles with grid

--- footer.php
+++ footer.php
@@ -1,106 +1,213 @@
 <footer class='footer'>
-    <div class="container footer-wrapper">
-       <div class="footer-logo-socials-wrapper">
-            <div>
-                <div class="footer-logo-wrapper">
-                    <?php 
-                            if ( has_custom_logo() ) {
-                                echo get_custom_logo();
-                            }
-                        ?>
+    <div class="container">
+       <div class="footer-wrapper">
+            <div class="footer-logo-socials-wrapper">
+                    <div class="footer-logo-description-wrapper">
+                        <div class="footer-logo">
+                            <?php 
+                                    if ( has_custom_logo() ) {
+                                        echo get_custom_logo();
+                                    }
+                                ?>
+                            
+                            <a class="footer-logo-link" href="<?php echo site_url(''); ?>">
+                                <p class="footer-logo-name title-h3">
+                                    Архангел світла
+                                </p>
+                            </a>
+                        </div>
+                        <p class="footer-logo-description">
+                            <?php the_field( 'footer_logo_description', 'option' ); ?>
+                        
+                        </p>    
+                    </div>
                     
-                    <a class="footer-logo-link" href="<?php echo site_url(''); ?>">
-                        <p class="footer-logo-name title-h3">
-                            Архангел світла
-                        </p>
-                    </a>
-                </div>
-                <p class="footer-logo-description">
-                    <?php the_field( 'footer_logo_description', 'option' ); ?>
-                
-                </p>    
+                    <div class="socials-wrapper">
+                        <h6 class="title-h6 footer-heading footer-heading-socials">
+                            Соціальні мережі
+                        </h6>
+                        <ul class="socials">
+                            <?php if ( get_field( 'instagram', 'option' ) ) { ?>
+                                <li class="social-item">
+                                    <a class="social-link" href="<?php the_field( 'instagram', 'option' ); ?>"
+                                        target="_blank">
+                                    
+                                            <svg width="19" height="19">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-instagram"></use>
+                                            </svg>
+                                    
+                                    </a>
+                                </li>
+                            <?php } ?>
+                            <?php if ( get_field( 'facebook', 'option' ) ) { ?>
+                                <li class="social-item">
+                                    <a class="social-link" href="<?php the_field( 'facebook', 'option' ); ?>"
+                                        target="_blank">
+                                        
+                                            <svg width="11" height="19">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-facebook"></use>
+                                            </svg>
+                                
+                                    </a>
+                                </li>
+                            <?php } ?>
+                            <?php if ( get_field( 'twitter', 'option' ) ) { ?>
+                                <li class="social-item">
+                                    <a class="social-link" href="<?php the_field( 'twitter', 'option' ); ?>"
+                                        target="_blank">
+                                        
+                                            <svg width="20" height="18">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-twitter-x"></use>
+                                            </svg>
+                                
+                                    </a>
+                                </li>
+                            <?php } ?>
+                            <?php if ( get_field( 'linkedin', 'option' ) ) { ?>
+                                <li class="social-item">
+                                    <a class="social-link" href="<?php the_field( 'linkedin', 'option' ); ?>"
+                                        target="_blank">
+                                    
+                                            <svg width="20.5" height="20.5">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-linkedIn"></use>
+                                            </svg>
+                                    
+                                    </a>
+                                </li>
+                            <?php } ?>
+                            <?php if ( get_field( 'telegram', 'option' ) ) { ?>
+                                <li class="social-item">
+                                    <a class="social-link" href="<?php the_field( 'telegram', 'option' ); ?>"
+                                        target="_blank">
+                                    
+                                            <svg width="20.34" height="17.78">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-telegram"></use>
+                                            </svg>
+                                    
+                                    </a>
+                                </li>
+                            <?php } ?>
+                            <?php if ( get_field( 'youtube', 'option' ) ) { ?>
+                                <li class="social-item">
+                                    <a class="social-link" class='' href="<?php the_field( 'youtube', 'option' ); ?>"
+                                        target="_blank">
+                                    
+                                            <svg width="20.73" height="15.11">
+                                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-youtube"></use>
+                                            </svg>
+                                    
+                                    </a>
+                                </li>
+                            <?php } ?>
+                            
+                        </ul>
+                    </div>
             </div>
-            
-            <div class="socials-wrapper">
-                <h6 class="title-h6 footer-heading-socials">
-                    Соціальні мережі
-                </h6>
-                <div class="socials">
-                    
+            <div class="footer-menu-wrapper">
+                <div class="footer-menu-heading-wrapper">
+                    <h6 class="title-h6 footer-heading">Посилання</h6>
+                    <div class="footer-menu-heading-icon-wrapper">
+                        <svg class="footer-menu-heading-icon" width="13.82" height="8">
+                            <use
+                                href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-arrow">
+                            </use>
+                        </svg>
+                    </div>
+                </div>
+
+                <div class="footer-menu-list-wrapper">
+                    <?php $menu = wp_nav_menu( [
+                                'theme_location' => 'footer',
+                                'container'      => false,
+                                'menu_class'     => "menu-list",
+                                'menu_id'        => false,
+                                'echo'           => true,
+                                'items_wrap'     => '<ul id="%1$s" %2$s">%3$s</ul>',
+                            ] );
+                    if($menu) : ?>
+
+                        <nav>
+                            <?php echo $menu ?>	
+                        </nav>
+
+                    <?php endif; ?>    
                 </div>
             </div>
-
-       </div>
-        <div class="footer-menu-wrapper">
-            <h6 class="title-h6 footer-heading">Посилання</h6>
-
-            <?php $menu = wp_nav_menu( [
-						'theme_location' => 'footer',
-						'container'      => false,
-						'menu_class'     => "menu-list",
-						'menu_id'        => false,
-						'echo'           => true,
-						'items_wrap'     => '<ul id="%1$s" %2$s">%3$s</ul>',
-					] );
-			if($menu) : ?>
-
-                <nav>
-                    <?php echo $menu ?>	
-                </nav>
-
-            <?php endif; ?>    
-
-        </div>
-        <div class="contacts">
-            <h6 class="title-h6 footer-heading">Наші контакти</h6>
-               <div class="contacts-item">
-                    <a href="mailto:<?php the_field( 'email', 'option' ); ?>"
+            <div class="contacts">
+                <h6 class="title-h6 footer-heading footer-heading-not-socials">Наші контакти</h6>
+                <div class="contacts-item">
+                    <a class="contacts-link" href="mailto:<?php the_field( 'email', 'option' ); ?>"
                             target="_blank">
-                        <!-- <span class="icon"
-                                style="background: url('<?php bloginfo( 'template_url' ); ?>/assets/images/colorIcon-envelope.svg');"></span> -->
+                        <span class="contact-icon"> 
+                            <svg width="18.78" height="15.09">
+                                <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-envelope"></use>
+                            </svg>
+                        </span>
                         <p class=""><?php the_field( 'email', 'option' ); ?></p>
                     </a>
-               </div>
+                </div>
 
                 <div class="contacts-item">
-                    <a href="<?php the_field('address_google_link', 'options' ); ?>"
+                    <a class="contacts-link" href="<?php the_field('address_google_link', 'options' ); ?>"
                                 target="_blank">
-                                    
+                        <span class="contact-icon"> 
+                            <svg width="17.01" height="20.56">
+                                <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-location"></use>
+                            </svg>
+                        </span>
                         <p class=""><?php the_field('address', 'options' ); ?></p>
                     </a>
                 </div>
-                <div class="contacts-item"><p class=""><?php the_field('work_hours', 'options' ); ?></p></div>
                 <div class="contacts-item">
-                    <a href="tel:<?php the_field( 'hotline_phone', 'option' ); ?>"
-                            target="_blank">
-                        <!-- <span class="icon"
-                                style="background: url('<?php bloginfo( 'template_url' ); ?>/assets/images/colorIcon-phone.svg');"></span> -->
-                        <p>
-                            <span class=""><?php the_field( 'hotline_phone', 'option' ); ?></span>
-                            <span><?php the_field( 'hotline_phone_text', 'option' ); ?></span>
-                        </p>
-                    </a>
-                    <a href="tel:<?php the_field( 'help_phone', 'option' ); ?>"
-                            target="_blank">
-                        <p>
-                            <span class=""><?php the_field( 'help_phone', 'option' ); ?></span>
-                            <span><?php the_field( 'help_phone_text','option' ); ?></span>
-                        </p>
-                    </a>
+                    <div class="contacts-link">
+                        <span class="contact-icon"> 
+                            <svg width="18.5" height="18.5">
+                                <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-clock"></use>
+                            </svg>
+                        </span>
+                        <p class=""><?php the_field('work_hours', 'options' ); ?></p>
+                    </div>
                 </div>
+                <div class="contacts-item">
+                    <div class="contacts-link">
+                        <span class="contact-icon"> 
+                            <svg width="18.5" height="18.5">
+                                <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-phone"></use>
+                            </svg>
+                        </span>
+                        <div class="contacts-phones">
+                            <a href="tel:<?php the_field( 'hotline_phone', 'option' ); ?>"
+                                    target="_blank">
+                                
+                                <p>
+                                    <span class=""><?php the_field( 'hotline_phone', 'option' ); ?></span>
+                                    <span><?php the_field( 'hotline_phone_text', 'option' ); ?></span>
+                                </p>
+                            </a>
+                            <a href="tel:<?php the_field( 'help_phone', 'option' ); ?>"
+                                    target="_blank">
+                                <p>
+                                    <span class=""><?php the_field( 'help_phone', 'option' ); ?></span>
+                                    <span><?php the_field( 'help_phone_text','option' ); ?></span>
+                                </p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="contact-us">
+                <h6 class="title-h6 footer-heading footer-heading-not-socials">Зв’яжіться з нами</h6>
+                <a href="/" class="footer-button">Стати партнером</a>
+                <a href="/" class="footer-button">Стати волонтером</a>
+                <a href="/" class="footer-button">Запит на допомогу</a>
+            </div>
+       </div>
+       <div class="footer-copyright-wrapper">
+                <p class="footer-copyright-text">© By using this website, you agree to the Terms of Service and Privacy Policy  </p>
+            </div>
 
-        </div>
-        <div class="contact-us">
-            <h6 class="title-h6 footer-heading">Зв’яжіться з нами</h6>
-            <a href="/" class="footer-button">Стати партнером</a>
-            <a href="/" class="footer-button">Стати волонтером</a>
-            <a href="/" class="footer-button">Запит на допомогу</a>
-        </div>
-
-
-        
     </div>
-    
+      
 </footer>
 <?php wp_footer(); ?>  
 </body>
