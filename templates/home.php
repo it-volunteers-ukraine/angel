62b9602b7d5f0484d5490390b72650fd588d8302
Olya <katniss30051991@gmail.com>
2024-03-27 13:50:59 +0200

    Start work

--- templates/home.php
+++ templates/home.php
@@ -4,7 +4,27 @@ Template Name: home
 */
 get_header();
 ?>
-<h2>home page</h2>
+<main>
+    <section class="hero">
+        <div class="container">
+            <div>
+                <h2>
+                    <?php the_field("hero_sub_title")?>
+                </h2>
+                <h1>
+                    <?php the_field("hero_main_title")?>
+                </h1>
+                <p>
+                    <?php the_field("hero_text")?>
+                </p>
+                <img src="<?php the_field("hero_image")?>" alt="" />
+                <a href="#"><?php the_field("hero_btn_text")?></a>
+            </div>
+
+        </div>
+
+    </section>
+</main>
 
 
 
