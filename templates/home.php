4dfa2ca39db8963a8db07fa0927c8f1211c21314
Olya <katniss30051991@gmail.com>
2024-03-29 20:58:40 +0200

    Add hero section

--- templates/home.php
+++ templates/home.php
@@ -6,23 +6,35 @@ get_header();
 ?>
 <main>
     <section class="hero">
-        <div class="container">
-            <div>
-                <h2>
+        <div class="container hero-container">
+            <div class="hero-image-wrapper">
+                <?php  if (get_field( 'hero_image' )) {?>
+                <img class="hero-image" src="<?php echo esc_url( get_field( 'hero_image' )['url'] ); ?>"
+                    alt="<?php echo esc_attr( get_field( 'hero_image' )['alt'] ); ?>" />
+                <?php } ?>
+            </div>
+            <div class="hero-text-wrapper">
+                <?php  if (get_field( 'hero_sub_title' )) {?>
+                <h2 class="hero-sub-title">
                     <?php the_field("hero_sub_title")?>
                 </h2>
-                <h1>
+                <?php } ?>
+                <?php  if (get_field( 'hero_main_title' )) {?>
+                <h1 class="title-h1">
                     <?php the_field("hero_main_title")?>
                 </h1>
-                <p>
+                <?php } ?>
+                <?php  if (get_field( 'hero_text' )) {?>
+                <div class="hero-text">
                     <?php the_field("hero_text")?>
-                </p>
-                <img src="<?php the_field("hero_image")?>" alt="" />
-                <a href="#"><?php the_field("hero_btn_text")?></a>
+                </div>
+                <?php } ?>
+                <?php  if (get_field( 'hero_btn_text' )) {?>
+                <a href=<?php the_field("hero_btn_link")?>
+                    class="primary-button hero-btn"><?php the_field("hero_btn_text")?></a>
+                <?php } ?>
             </div>
-
         </div>
-
     </section>
 </main>
 
