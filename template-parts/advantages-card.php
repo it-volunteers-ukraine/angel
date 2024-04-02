24fc46c5c0ccf99ff67dea7e7451fb8640a00a5a
Olya <katniss30051991@gmail.com>
2024-04-02 20:48:58 +0300

    Changed styles for advantage cards

--- template-parts/advantages-card.php
+++ template-parts/advantages-card.php
@@ -6,7 +6,12 @@ $description   = get_sub_field( 'description' );
 ?>
 
 <li class="card">
-    <img src="<?php echo esc_url( $icon['url'] ); ?>" alt="<?php echo esc_attr( $icon['alt'] ); ?>" />
+    <svg>
+        <use
+            href="<?php bloginfo( 'template_url' ); ?>/assets/images/sprite.svg#<?php echo esc_attr( $icon['alt'] ); ?>">
+        </use>
+    </svg>
+    <!-- <img src="<?php echo esc_url( $icon['url'] ); ?>" alt="<?php echo esc_attr( $icon['alt'] ); ?>" /> -->
     <p class="counter"><?php echo $counter; ?></p>
     <p class="description"><?php echo $description; ?></p>
 
