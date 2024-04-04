5bf18a04360a2149b49881815c76e86ba693e106
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-04-04 13:50:17 +0300

    activity of the fond - beginning

--- functions.php
+++ functions.php
@@ -30,9 +30,9 @@ function wp_it_volunteers_scripts() {
     wp_enqueue_script( 'home-scripts', get_template_directory_uri() . '/assets/scripts/template-scripts/home.js', array(), false, true );
   }
 
-  if ( is_page_template('templates/about.php') ) {
-    wp_enqueue_style( 'about-style', get_template_directory_uri() . '/assets/styles/template-styles/about.css', array('main') );
-    wp_enqueue_script( 'about-scripts', get_template_directory_uri() . '/assets/scripts/template-scripts/about.js', array(), false, true );
+  if ( is_page_template('templates/activity.php') ) {
+    wp_enqueue_style( 'about-style', get_template_directory_uri() . '/assets/styles/template-styles/activity.css', array('main') );
+    wp_enqueue_script( 'about-scripts', get_template_directory_uri() . '/assets/scripts/template-scripts/activity.js', array(), false, true );
   }
 
   if ( is_page_template('templates/contacts.php') ) {
--- templates/activity.php
+++ templates/activity.php
@@ -4,7 +4,37 @@ Template Name: Activity
 */
 get_header();
 ?>
-<h2>Діяльність нашого фонду</h2>
+<main>
+    <section class="activity">
+       <div class="container">
+            <h2 class="title-h5"><?php the_field( 'about_fond_work_directions_title' ); ?></h2>
+                <?php if( have_rows('about_fond_work_directions_list') ): ?>
+                    
+                    <ul class="work-directions-list">
+
+                        <?php while( have_rows('about_fond_work_directions_list') ): the_row(); ?>
+
+                            <li class="work-directions-item"><?php the_sub_field('about_fond_work_directions_item'); ?></li>
+                            
+                        <?php endwhile; ?>
+
+                    </ul>
+
+                <?php endif; ?>
+
+            <?php if ( get_field( 'about_fond_name_title' ) ) { ?>
+                <h2 class="title-h5"><?php the_field( 'about_fond_name_title' ); ?></h2>
+            <?php } ?>
+
+            <?php if ( get_field( 'about_fond_name_text' ) ) { ?>
+            <p><?php the_field( 'about_fond_name_text' ); ?></p>
+            <?php } ?>
+       </div>
+    </section>
+   
+
+
+</main>
 
 
 
