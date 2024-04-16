b47c7f74f7b16be4ad36526023148f72137a75aa
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-04-16 18:03:37 +0300

    add fund goals accordion on mobile

--- src/scripts/template-scripts/activity.js
+++ src/scripts/template-scripts/activity.js
@@ -1,14 +1,22 @@
-const aboutArrowBtn = document.querySelector(".about-heading-icon-wrapper");
+const aboutArrowBtn = document.querySelector(".about-heading-wrapper .about-heading-icon-wrapper");
 const conetentEls = document.querySelectorAll(".toggle-open-close-content");
-const iconEl = document.querySelector(".about-heading-icon");
+const iconEl = document.querySelector(".activity .about-heading-wrapper .about-heading-icon");
 
-           
+const goalsArrowBtnEl = document.querySelector(".activity .sub-info .about-heading-icon-wrapper ");
+const goalsListEl = document.querySelector(".activity .sub-info ul");
+const goalsIconEl = document.querySelector(".activity .sub-info .about-heading-icon");
+
+
 
 
 
 aboutArrowBtn.addEventListener("click", () => {
     conetentEls.forEach((el) => el.classList.toggle("is-open"));
     iconEl.classList.toggle("about-icon-rotate");
-
 });
-;
+
+goalsArrowBtnEl.addEventListener("click", ()=>{
+    goalsListEl.classList.toggle("is-open")
+    goalsIconEl.classList.toggle("about-icon-rotate");
+
+})
