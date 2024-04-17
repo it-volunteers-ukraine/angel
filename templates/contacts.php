ec01fc647e88125a829e825a74809232c7ac6117
Oksana Baranik <ksyubaranik@gmail.com>
2024-04-17 07:33:49 +0200

    Add image, add icons

--- templates/contacts.php
+++ templates/contacts.php
@@ -13,7 +13,7 @@ get_header();
                     <?php 
                     $image = get_field('contact_image');
                     if( !empty( $image ) ): ?>
-                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>"/>
                     <?php endif; ?>
                 </div>
                 <div class="contact-us__info">
@@ -24,9 +24,11 @@ get_header();
                         <h5 class="title-h5"><?php the_field('our_address_title'); ?></h5>
                         <div class="info__item">
                             <a class="info__link" href="<?php the_field('address_google_link', 'options' ); ?>" target="_blank">
-                                <svg class="info__icon" width="24" height="24">
-                                    <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/footer-sprite.svg#icon-location"></use>
-                                </svg>
+                                <span class="info__icon">
+                                    <svg width="24" height="24">
+                                        <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-location"></use>
+                                    </svg>
+                                </span>
                                 <p class="info__link-item short"><?php the_field('address', 'options' ); ?></p>
                             </a>
                         </div>
@@ -36,9 +38,11 @@ get_header();
                         <h5 class="title-h5"><?php the_field('work_schedule_title'); ?></h5>
                         <div class="info__item">
                             <div class="info__link" >
-                                <svg class="info__icon" width="24" height="24">
+                                <span class="info__icon">
+                                    <svg width="24" height="24">
                                         <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-clock"></use>
-                                </svg>
+                                    </svg>
+                                </span>
                                 <p><?php the_field('work_hours', 'options' ); ?></p>
                             </div>
                         </div>
@@ -48,17 +52,21 @@ get_header();
                         <h5 class="title-h5"><?php the_field('contacts_for_communication_title'); ?></h5>
                         <div class="info__item">
                             <a class="info__link" href="mailto:<?php the_field( 'email', 'option' ); ?>" target="_blank">
-                                <svg class="info__icon" width="24" height="24">
-                                    <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-envelope"></use>
-                                </svg>
+                                <span class="info__icon">
+                                    <svg width="24" height="24">
+                                        <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-envelope"></use>
+                                    </svg>
+                                </span>
                                 <p class="info__link-item"><?php the_field( 'email', 'option' ); ?></p>
                             </a>
                         </div>
                         <div class="info__item-tel">
                             <div class="info__link">
-                                <svg class="info__icon" width="24" height="24">
-                                    <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-phone"></use>
-                                </svg>
+                                <span class="info__icon">
+                                    <svg width="24" height="24">
+                                        <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/footer-sprite.svg#icon-phone"></use>
+                                    </svg>
+                                </span>
                                 <div>
                                     <a href="tel:<?php the_field( 'phone_number_1' ); ?>" target="_blank">
                                         <p class="info__link-item">
@@ -79,12 +87,8 @@ get_header();
         </div>
     </section>
     <section class="contact__persons section">
-        <div class="container">
-
-        </div>
+        <?php get_template_part('template-parts/contact-persons-section'); ?>
     </section>
-
-
 </main>
 
 
