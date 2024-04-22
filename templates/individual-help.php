556e2e4777b74cc5c53aaaf9ee5d8d931dc45eb2
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-04-22 22:45:26 +0300

    individual-help page first section and second section beginning

--- templates/individual-help.php
+++ templates/individual-help.php
@@ -4,7 +4,51 @@ Template Name: Individual help
 */
 get_header();
 ?>
-<h2>Індивідуальна допомога</h2>
+
+<main>   
+    <section class="section individual-help"> 
+        <div class="container">
+            <?php  if (get_field( 'first_title' )) {?>
+                <h2 class="page-title title-h2"><?php the_field("first_title")?></h2>
+            <?php } ?>
+            <div class="individual-help-info">
+                <div class="help-image-wrapper">
+                    <?php  if (get_field( 'help_image' )) {?>
+                    <img class="help-image" src="<?php echo esc_url( get_field( 'help_image' )['url'] ); ?>"
+                        alt="<?php echo esc_attr( get_field( 'help_image' )['alt'] ); ?>" />
+                    <?php } ?>
+                </div>
+                <div class="help-text">
+                    <?php  if (get_field( 'help_text' )) {?>
+                        
+                            <?php the_field("help_text")?>
+                        
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section need-help">
+        <div class="container">
+            <?php  if (get_field( 'second_title' )) {?>
+                <h2 class="page-title title-h2"><?php the_field("second_title")?></h2>
+            <?php } ?>
+            <p class="title-h5 second-text ">
+                <?php  if (get_field( 'second_text' )) {?>
+                    
+                        <?php the_field("second_text")?>
+                    
+                <?php } ?>
+            </p>
+
+            <div>
+                
+            </div>
+        </div>
+        
+    </section>
+</main>
+
 
 
 
