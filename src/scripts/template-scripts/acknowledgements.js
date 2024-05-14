07b4db419ccd33cea159b806000fd8353fcffd45
Oksana Baranik <ksyubaranik@gmail.com>
2024-05-14 10:36:30 +0200

    Add ajax

--- src/scripts/template-scripts/acknowledgements.js
+++ src/scripts/template-scripts/acknowledgements.js
@@ -1,14 +1,66 @@
-// Отримати ширину екрану
-var screenWidth = window.innerWidth;
+jQuery(document).ready(function ($) {
+    
+  // Отримання nonce
+  function getAcknowledgementsNonce() {
+    return myAjax.nonce;
+  }
 
-// Відправити ширину екрану на сервер через AJAX
-var xhr = new XMLHttpRequest();
-xhr.open('POST', 'handle-screen-width.php', true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log('Width sent successfully.');
-        // Тут ви можете обробити відповідь, якщо це необхідно
-    }
-};
-xhr.send('screenWidth=' + screenWidth);
+  // Початок скрипту
+  var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  var page = 1;
+
+  // Функція для виведення постів через AJAX
+  function load_posts(page) {
+    var data = {
+      action: "load_acknowledgements",
+      nonce: getAcknowledgementsNonce(),
+      width: viewportWidth,
+      page: page,
+    };
+    $.ajax({
+      url: myAjax.ajaxUrl,
+      type: "POST",
+      data: data,
+      success: function (response) {
+        $(".acknowledgements__content__cards").html(response.posts);
+      },
+      error: function (xhr, status, error) {
+        console.error("Request failed: " + error);
+      },
+    }); 
+  }
+
+  // Функція для виведення пагінації через AJAX
+  function load_pagination(page) {
+    var data = {
+      action: "load_pagination",
+      nonce: getAcknowledgementsNonce(),
+      width: viewportWidth,
+      page: page,
+    };
+    $.ajax({
+      url: myAjax.ajaxUrl,
+      type: "POST",
+      data: data,
+        success: function(response) {
+            $(".pagination").html(response.pagination);
+        },
+        error: function(xhr, status, error) {
+            console.error('Request failed: ' + error);
+        }
+    });
+  }
+
+   // Завантаження постів та пагінації при завантаженні сторінки
+   load_posts(page);
+   load_pagination(page);
+
+   // Обробник кліків на сторінки пагінації
+  $(document).on('click', '.pagination a', function(e) {
+    e.preventDefault();
+    var page = $(this).data('paged') || $(this).attr('href').split('paged=')[1]; // Отримуємо номер сторінки з посилання
+    load_posts(page);
+    load_pagination(page);
+    console.log("hi");
+});
+})
