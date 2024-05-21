fd756827bd67e149adc7649765ab0c0644ad7f24
Olha Bukina <olha.bukina91@gmail.com>
2024-05-21 18:47:46 +0300

    Fix pagination

--- template-parts/pagination.php
+++ template-parts/pagination.php
@@ -10,7 +10,7 @@ $total_pages = $args["total_pages"];
             'current' => $current_page,
             'total' => $total_pages,
             'prev_next' => false,
-            'show_all' => true,
+            'show_all' => false,
             'end_size' => 1,
             'mid_size' => ($current_page === 1) || ($current_page == $total_pages) ? 3 : 1,
         ]);
