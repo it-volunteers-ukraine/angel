6e808216180c5a152d8e0f85e222c5026192ca95
Olga Barnikova <barnikovaolga8@gmail.com>
2024-05-22 13:23:00 +0300

    fixed single-product

--- woocommerce/wc-functions-remove.php
+++ woocommerce/wc-functions-remove.php
@@ -7,9 +7,11 @@ remove_all_filters( 'woocommerce_before_main_content');
 remove_all_filters( 'woocommerce_after_single_product_summary');
 //remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
 //remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
 
 
-//custom-auction-frontend.js
+
+//connected custom-auction-frontend.js
 add_action( 'wp_enqueue_scripts', 'theme_custom_auction_script', 99999 );
 
 if (!function_exists('theme_custom_auction_script')) {
