647cf56be4d5aeefa31d7605b9abd9813e89aee4
Oksana Baranik <ksyubaranik@gmail.com>
2024-05-22 09:49:33 +0200

    Fixed container for auction-slider

--- template-parts/auction-slider.php
+++ template-parts/auction-slider.php
@@ -1,57 +1,28 @@
 <section class="auction section">
     <div class="container">
         <h2 class="page-title title-h2"><?php the_field('charity_auction_title', 'option')?></h2>
-        <!-- Слайдер -->
+        <!-- Slider Swiper -->
         <div class="auction-slider swiper-container">
             <ul class="auction-slider__wrapper swiper-wrapper">
-                <?php 
-                $category = $args["category"];
-                $args = array(
+            <?php 
+                $category = isset($args["category"]) ? $args["category"] : '';
+                $query_args = array(
                     'post_type'      => 'product',
-                    'posts_per_page' => 6, 
-                    'category_name' => $category, 
-                    'paged'          => get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1,
+                    'posts_per_page' => -1, 
+                    'category_name'  => $category, 
                 );
                 
-                $products = new WP_Query( $args );
-                $total_pages  = $products->max_num_pages;
-                $current_page = max( 1, get_query_var( 'paged' ) );                    
-
-                if ( $products->have_posts() ) {
-                    while ( $products->have_posts() ) {
+                $products = new WP_Query($query_args);
+                if ($products->have_posts()) {
+                    while ($products->have_posts()) {
                         $products->the_post();
-                        $product = wc_get_product( get_the_ID() ); // Отримуємо об'єкт продукту
+                        $product = wc_get_product(get_the_ID()); 
 
-                        if ( !$product ) {
-                            continue; // Пропускаємо ітерацію, якщо об'єкт продукту не вдалося отримати
+                        if (!$product) {
+                            continue; 
                         }
-                        ?>
                         
-                            <li class="product swiper-slide"<?php wc_product_class(); ?>>
-                                <?php
-                                echo '<a class="product__link" href="' . esc_url( get_permalink() ) . '">';
-                                    echo '<div class="product__img">';
-                                        do_action( 'woocommerce_before_shop_loop_item_title' );
-                                    echo '</div>';
-                                    echo '<div class="product__info">';                                    
-                                        do_action( 'woocommerce_shop_loop_item_title' );
-                                        echo '<div class="product__description">';
-                                            echo apply_filters( 'the_content', $product->get_description() );
-                                        echo '</div>';  
-                                        echo '<div class="auction__price">';
-                                        $auction_start_price = get_post_meta( get_the_ID(), '_auction_start_price', true );                                       
-                                        echo '<p>Поточна ставка: ' . $auction_start_price . '</p>';                                       
-                                        echo '</div>';
-                                        echo '<div class="auction-time">';
-                                        woocommerce_auction_countdown();
-                                        echo '</div>'; 
-                                        echo '<button class="product__button primary-button">' . esc_html( get_field('projects-btn-support', 'option') ) . '</button>';
-                                    echo '</div>';                                
-                                echo '</a>';
-                                ?>
-                            </li>
-                        
-                        <?php
+                        get_template_part('template-parts/auction-card', null, array('product' => $product, 'isSliderCard' => true));
                     }
                     wp_reset_postdata();
                 }
