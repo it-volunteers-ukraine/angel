0151d47a545b48b7ba7864a70739c272eaf776b4
Oksana Baranik <ksyubaranik@gmail.com>
2024-05-31 16:35:11 +0200

    Fixed pagination in auction-slider

--- src/scripts/template-parts-scripts/auction-slider.js
+++ src/scripts/template-parts-scripts/auction-slider.js
@@ -3,34 +3,32 @@ const auctionSwiper = new Swiper('.auction-slider', {
     nextEl: '.button-next',
     prevEl: '.button-prev',
   },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  grabCursor: true,
   lazy: {
     loadOnTransitionStart: true,
     loadPrevNext: true,
   },
   lazyPreloadPrevNext: 1,
-  slidesPerView: 1,
-  slidesPerGroup: 1, 
-  spaceBetween: 20,
+  slidesPerView: 2,
+  slidesPerGroup: 2, 
+  spaceBetween: 32,
   breakpoints: {
-    991: {
+    993: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 24,
+    },
+    769: {
       slidesPerView: 1.33,
       slidesPerGroup: 1,
-      spaceBetween: 24,
+      spaceBetween: 20,
     },
-    1200: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 24,
-    },
-    1919: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 32,
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 20,
     }  
+  },
+  pagination: {
+    el: '.swiper-pagination',
   },
 });
