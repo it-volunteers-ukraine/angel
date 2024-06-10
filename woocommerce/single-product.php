855d9067f3e5081834f85896a1cce3783b0a7159
Olga Barnikova <barnikovaolga8@gmail.com>
2024-06-10 12:37:05 +0300

    added styles for share-block

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -60,13 +60,17 @@ get_header( 'shop' ); ?>
             		echo '</p>';
 					?>
 					<p class="description"><?php the_field( 'reserve-price-info', 'option' ); ?></p>
-					<div class="share">
-						<p class="share__text">Поділіться цим аукціоном у соціальних мережах:</p>
-						<?php get_template_part('template-parts/share',); ?>	
+					<div class="share">					
+						<p class="share__text"><?php the_field('auction-share', 'option'); ?></p>
+						<?php get_template_part('template-parts/share'); ?>	
 					</div>
 				</div>
 				<div id="contactForm" name="lot-form" class="form" method="post">
               		<?php echo do_shortcode('[contact-form-7 id="cbb87ee" title="Форма для сторінки Лоту"]') ?>
+				</div>
+				<div class="share__mobile">					
+					<p class="share__text"><?php the_field('auction-share', 'option'); ?></p>
+					<?php get_template_part('template-parts/share'); ?>	
 				</div>
 			</div>
 			<div class="back"> 
@@ -75,7 +79,7 @@ get_header( 'shop' ); ?>
 		</div>
 	</section>	
 	<section class="project__slider section">
-		<?php get_template_part('template-parts/projects-slider',); ?>	
+		<?php get_template_part('template-parts/projects-slider'); ?>	
 	</section>
 	<?php
 		/**
