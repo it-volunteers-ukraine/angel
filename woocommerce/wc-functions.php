4947e2c194970e872858af69aa4ecbc2b9334157
Olga Barnikova <barnikovaolga8@gmail.com>
2024-06-11 13:04:32 +0300

    added elements in lot decription

--- woocommerce/wc-functions.php
+++ woocommerce/wc-functions.php
@@ -78,3 +78,29 @@ function auction_watchlist_count_shortcode() {
     return '<p class="wishlist-count">' . sprintf(__('%d', 'your-textdomain'), $count) . '</p>';
 }
 add_shortcode('auction_watchlist_count', 'auction_watchlist_count_shortcode');
+
+
+// LOT DESCRIPTIONS
+//Removing elements to reorder them
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+
+// Add elements in a new order
+add_action( 'woocommerce_single_product_summary', 'display_auction_price', 10 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 15 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 20 );
+
+// Function to display auction information
+function display_auction_price() {
+    echo '<div class="auction__price">';
+    $auction_start_price = get_post_meta( get_the_ID(), '_auction_start_price', true );
+    $currency = get_woocommerce_currency_symbol();                                       
+    echo '<p class="starting__bid">' . get_field('starting-bid', 'option') . '</p><p class="size">' . $auction_start_price . '</p><p class="size">' . $currency . '</p>';
+    echo '</div>';
+}
+
+// Function to display text before auction form
+function display_bid_prompt_before_form() {
+    echo '<p class="increase__bid">' . get_field('increase-bid', 'option') . '</p>';
+}
+add_action( 'woocommerce_before_bid_form', 'display_bid_prompt_before_form' );
