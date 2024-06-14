5acd2da0bfd060e1e34fa3d0ec18d2230f0b1576
Oksana Baranik <ksyubaranik@gmail.com>
2024-06-14 12:18:12 +0200

    Added markup for popup

--- templates/acknowledgements.php
+++ templates/acknowledgements.php
@@ -20,8 +20,29 @@ get_header();
                 </div>   
             </div>
         </div>
+        <div id="popup" class="popup">
+        <div class="popup__body">
+            <div class="popup__content">
+                <div class="popup__close">
+                    <a href=""></a>
+                </div>
+                
+                <?php
+                    $acknowledgementImg = get_field('acknowledgement_img');
+                ?>
+                <div class="card__img">
+                    <?php if (!empty($acknowledgementImg)) : ?>
+                        <a href="<?php echo esc_url($acknowledgementImg['url']); ?>">
+                            <img src="<?php echo esc_url($acknowledgementImg['url']); ?>" alt="<?php echo esc_attr($acknowledgementImg['alt']); ?>"/>
+                        </a>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
     </section>
     
 </main>  
+
 
 <?php get_footer(); ?>
